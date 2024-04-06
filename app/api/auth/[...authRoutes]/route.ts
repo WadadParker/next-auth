7b4697505bafcdd86ba6e92cfbb9dc64882d1918
Wadad Parker <wadadparker5@gmail.com>
2024-04-06 09:37:30 +0530

    feat: extract route from url

--- app/api/auth/[...authRoutes]/route.ts
+++ app/api/auth/[...authRoutes]/route.ts
@@ -3,13 +3,17 @@
 // 2] Add a folder with name as follow "[...routeHandlerName]"
 // 3] Now add route.ts file to the ...routeHandler folder you created
 // IMPORTANT: The handler for api/auth, you need to create route.ts file in api/auth folder
+// The dynamic [...] route folder also holds true for frontend routes
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
+export function GET(req:NextRequest , { params: { authRoutes } }: { params:{authRoutes: string[]} }) {
+    console.log(authRoutes);
     return NextResponse.json({
         message:"This is from next js"
     })
 }
 
 // In above code, I wrote the route name as api/auth/sdadasdae it still give the GET request reponse
+// Also bhai, this is server, if you console log here, it appears in terminal not the browser
+// The params argument will be in this form { params: { authRoutes: [ 'ddwdw' ] } } , so make sure to destructure it
